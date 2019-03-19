10b63520cfe792fd1664cd933dbc1b6f3f548b8d
Ilya Piatrenka <petrenko.ilia@gmail.com>
2019-03-19 22:00:57 +0100

    Initial database connection

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const Koa = require('koa');
 const app = new Koa();
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 app.use(async ctx => {
-  ctx.body = 'Hello World';
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      ctx.body = results;
+      client.release();
+    } catch (err) {
+      console.error(err);
+      ctx.body = "Error " + err;
+    }
 });
 
 app.listen(PORT);
