ef5f6b7e02a0c6d58477b75962a9ed96c2c0f26f
Ilya Piatrenka <petrenko.ilia@gmail.com>
2019-06-22 11:04:18 +0200

    Remove users backdoor

--- src/server/index.js
+++ src/server/index.js
@@ -88,18 +88,6 @@ router.get('/secret', async (ctx) => {
   })(ctx);
 });
 
-router.get('/', async (ctx) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM users');
-      const results = { 'results': (result) ? result.rows : null};
-      ctx.body = results;
-      client.release();
-    } catch (err) {
-      console.error(err);
-      ctx.body = "Error " + err;
-    }
-})
 app.use(router.routes());
 app.listen(PORT);
 
