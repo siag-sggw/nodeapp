8a52e3fcd691e6daac5998ab80b14418c3bd15d2
Ilya Piatrenka <petrenko.ilia@gmail.com>
2019-06-23 12:41:49 +0200

    Add secure cookies

--- src/server/index.js
+++ src/server/index.js
@@ -15,7 +15,21 @@ app.use(cors({
     origin(ctx) { return ctx.request.get("Origin") },
     credentials: true
 }));
-app.use(session(app));
+
+const CONFIG = {
+  key: 'koa:sess', /** (string) cookie key (default is koa:sess) */
+  /** (number || 'session') maxAge in ms (default is 1 days) */
+  /** 'session' will result in a cookie that expires when session/browser is closed */
+  /** Warning: If a session cookie is stolen, this cookie will never expire */
+  maxAge: 86400000,
+  autoCommit: true, /** (boolean) automatically commit headers (default true) */
+  overwrite: true, /** (boolean) can overwrite or not (default true) */
+  httpOnly: false, /** (boolean) httpOnly or not (default true) */
+  signed: true, /** (boolean) signed or not (default true) */
+  rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
+  renew: true, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
+};
+app.use(session(CONFIG, app));
 
 app.use(bodyParser());
 
